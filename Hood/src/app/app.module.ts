d5bb5e518b79dba0c038cdfa5987a6107feb8228
Alice-Githui <gnyokabi.alice@gmail.com>
2021-06-06 00:39:37 +0300

    import forms module

--- Hood/src/app/app.module.ts
+++ Hood/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {HttpClientModule} from '@angular/common/http';
+import {FormsModule} from  '@angular/forms';   
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -13,12 +14,12 @@ import { NavbarComponent } from './navbar/navbar.component';
     AppComponent,
     NeighbourhoodsComponent,
     RegistrationComponent,
-    NavbarComponent
-  ],
+    NavbarComponent  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
